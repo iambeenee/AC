f46b58e9294d005b28aa30f494645ebc658c313a
admin <admin@YD5-01>
2021-12-28 09:34:10 +0900

    1228

---
+++ chapter02/src/com/yedam/java/연습문제/연습문제0203.java
@@ -0,0 +1,48 @@
+package com.yedam.java.연습문제;
+
+import java.util.Scanner;
+
+public class 연습문제0203 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		// 9
+		
+		/*
+		long var1 = 2L;
+		float var2 = 1.8f;
+		double var3 = 2.5;
+		String var4 = "3.9";
+		int result = (int)(var1 + var2 + var3
+		
+		System.out.println(result);
+		*/
+		
+		//chap 4-1
+		String name = "감자바";
+		int age = 25;
+		String tel1 = "010", tel2="123", tel3="4567";
+		System.out.println("이름 : " + name);
+		System.out.print("나이 : " + age + "\n");
+		System.out.printf("전화 : %1s-%2s-%3s", tel1, tel2, tel3);
+		
+		//chap 4-2
+		Scanner scanner = new Scanner(System.in);
+		
+		System.out.print("첫번째 수");
+		String strNum1 = scanner.nextLine();
+		
+		System.out.printf("두번째 수");
+		String strNum2 = scanner.nextLine();
+				
+		int num1 = I
+		int num2 = 
+		int result = num1 + num2;
+		System.out.println("덧셈 결과 : " + result);
+		
+	
+	}
+
+}
